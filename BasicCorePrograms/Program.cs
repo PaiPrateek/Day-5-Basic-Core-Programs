d05d62e419d82fd6171f3b53bb3c546d2fa7fd3c
Prateek Pai <paiprateek123@gmail.com>
2022-06-11 15:49:41 +0530

    Leap Year Program

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -6,8 +6,22 @@ namespace BasicCorePrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Basic Core Program");
+            CheckLeapYear();
         }
+        public static void CheckLeapYear()
+        {
+            Console.WriteLine("Enter A Year ");
+            int Year=Convert.ToInt32(Console.ReadLine());
+            if (Year%4==0 & (Year%100!=0 || Year%400==0))
+            {
+                Console.WriteLine("Entered Year {0} is a Leap Year", Year);
+            }
+            else
+            {
+                Console.WriteLine("Entered Year {0} is not a Leap Year", Year);
+            }
+        }
+
 
     }
 }
