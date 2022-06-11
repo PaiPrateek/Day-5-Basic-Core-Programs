47ad4a36d5da1a32837c4f91cf7319ae2b462d6f
Prateek Pai <paiprateek123@gmail.com>
2022-06-11 16:22:53 +0530

    Largest Among Three Numbers

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -7,8 +7,32 @@ namespace BasicCorePrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Basic Core Program");
+            LargestAmoungThreeNumber();
         }
+        public static void LargestAmoungThreeNumber()
+        {
+            Console.WriteLine("Enter First Number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("......................");
+            Console.WriteLine("Enter Second Number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("......................");
+            Console.WriteLine("Enter Third Number");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("......................");
 
+            if (a > b && a > c)
+            {
+                Console.WriteLine("First Entered Number {0} is Larger among three Number:", a);
+            }
+            else if (b > c && b > a)
+            {
+                Console.WriteLine("Second Entered Number {0} is Larger among three Number:", b);
+            }
+            else
+            {
+                Console.WriteLine("Third Entered Number {0} is Larger among three Number:", c);
+            }
+        }
     }
 }
