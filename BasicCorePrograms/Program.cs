de25b65aa51e0d6b2bbcf9821cef1e1da63dba20
Prateek Pai <paiprateek123@gmail.com>
2022-06-11 16:00:16 +0530

    Harmonic Number

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -6,7 +6,21 @@ namespace BasicCorePrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Basic Core Program");
+            CheckHarmonicNumber();
+        }
+        public static void CheckHarmonicNumber()
+        {
+            double Result = 0.0;
+            Console.WriteLine("Enter any Number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= Number; i++)
+            {
+
+                Console.Write("1/{0} + ", i);
+                Result += 1 / (double)i;
+
+            }
+            Console.WriteLine("Sum of Harmonic Number Series up to {0} is :{1} ", Number, Result);
         }
 
     }
