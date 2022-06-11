2d60de09a3666be238ecc556adca666ed194bc8f
Prateek Pai <paiprateek123@gmail.com>
2022-06-11 16:03:43 +0530

    Compute the Prime Factorization

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -6,7 +6,31 @@ namespace BasicCorePrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Basic Core Program");
+            CheckPrimeFactor();
+        }
+        public static void CheckPrimeFactor()
+        {
+            int i, j, Count = 0;
+            Console.WriteLine("Enter Any Number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= Number; i++)
+            {
+                if (Number % i == 0)
+                {
+                    Count = 0;
+                    for (j = 1; j <= i; j++)
+                    {
+                        if (i % j == 0)
+                            Count++;
+                    }
+                    if (Count == 2)
+                    {
+                        Console.WriteLine("Prime Factors are " + i);
+                    }
+
+                }
+            }
         }
 
     }
