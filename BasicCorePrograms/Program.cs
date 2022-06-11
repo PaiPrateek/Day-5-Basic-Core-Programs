a3015943c53ab56def757c26b8b27ae576ee66c9
Prateek Pai <paiprateek123@gmail.com>
2022-06-11 16:20:07 +0530

    Check Whether an Alphabet is Vowel or Consonant

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -7,8 +7,22 @@ namespace BasicCorePrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Basic Core Program");
+            CheckVowelOrConsonant();
         }
-
+        public static void CheckVowelOrConsonant()
+        {
+            char alpha;
+            Console.WriteLine("Enter any Alphabet to check whether it is Vowel or Consonant");
+            char Alph = Convert.ToChar(Console.ReadLine());
+            alpha = char.ToLower(Alph);
+            if ((alpha == 'a') || (alpha == 'e') || (alpha == 'i') || (alpha == 'o') || (alpha == 'u'))
+            {
+                Console.WriteLine("Entered Alphabet {0} is Vowel", Alph);
+            }
+            else
+            {
+                Console.WriteLine("Entered Alphabet {0} is Consonant", Alph);
+            }
+        }
     }
 }
