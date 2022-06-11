60e515798c17a6ec88d4f6dc387923087ea594c5
Prateek Pai <paiprateek123@gmail.com>
2022-06-11 16:07:07 +0530

    Compute Quotient and Remainder

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -6,7 +6,21 @@ namespace BasicCorePrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Basic Core Program");
+            ComputeQuotientAndReminder();
+        }
+        public static void ComputeQuotientAndReminder()
+        {
+            Console.WriteLine("Enter a Dividend:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("........................");
+            Console.WriteLine("Enter a Divisor:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            int Quotient = x / y;
+            int Reminder = x % y;
+
+            Console.WriteLine("The Quotient is :{0}", Quotient);
+            Console.WriteLine("The Reminder is : {0}", Reminder);
         }
 
     }
