7616becc3ff046620a0dc28012626a39463e646d
Prateek Pai <paiprateek123@gmail.com>
2022-06-11 16:16:55 +0530

    Check Whether a Number is Even or Odd

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -7,8 +7,20 @@ namespace BasicCorePrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Basic Core Program");
+            CheckEvenOrOdd();
         }
-
+        public static void CheckEvenOrOdd()
+        {
+            Console.WriteLine("Enter Any Number to check Whether it is Even or odd ");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            if (Number % 2 == 0)
+            {
+                Console.WriteLine("The Entered Number {0} is Even", Number);
+            }
+            else
+            {
+                Console.WriteLine("The Entered Number {0} is Odd", Number);
+            }
+        }
     }
 }
