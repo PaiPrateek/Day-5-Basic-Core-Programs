ce2e3d65246d82e01fa6d21705caa2e67ea8b0a4
Prateek Pai <paiprateek123@gmail.com>
2022-06-11 15:54:58 +0530

    Calculte the Power of Two Program

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -6,8 +6,26 @@ namespace BasicCorePrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Basic Core Program");
+            CheckPowerOfTwo();
         }
+        public static void CheckPowerOfTwo()
+        {
+            int Result = 1;
+            Console.WriteLine("Enter a Number to Calculate the Power of 2");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            if (Number <= 31)
+            {
+                for (int i = 0; i < Number; i++)
+                {
+                    Result *= 2;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please Enter a Number Less than 31");
+            }
 
+            Console.WriteLine("The Power of 2 of the Entered Number is :" + Result);
+        }
     }
 }
